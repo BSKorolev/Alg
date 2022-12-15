44de4a0cfb7caedbb1de81831d660d68508b9f14
Boria <BSKorolev@stud.kantiana.ru>
2022-12-15 10:29:12 +0200

    lab4

---
+++ lab4/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#define N 10
+
+int main() 
+{
+	int mas[N];
+	for (int i = 0; i < N; i++)
+		mas[i] = rand() % 100; //filling array
+	for (int i = 0; i < N; i++)
+		cout << mas[i] << endl; 
+	cout << "=================" << endl;
+	double factor = 1.2473309; //reduction factor
+	int k = N - 1; //step
+	while (k >= 1) 
+	{
+		for (int i = 0; i + k < N; i++) 
+		{
+			if (mas[i] > mas[i + k]) 
+			{
+				std::swap(mas[i], mas[i + k]);
+			}
+		}
+		k /= factor;
+	}
+	for (int i = 0; i < N; i++)
+		cout << mas[i] << endl;
+}
+// worst n^2
+// best n*logn
