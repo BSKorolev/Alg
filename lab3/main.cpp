145e31015ee7a37b46d61cbcc7014f8ca356efdd
Boria <BSKorolev@stud.kantiana.ru>
2022-12-15 10:01:32 +0200

    lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "¬ведите число:" << endl;
+	int x;
+	cin >> x;
+	int  n3 = 1; //берЄм числа равные 1 как K,L,M =0
+	while (n3 <= x) 
+	{
+		int n5 = 1;
+		while (n5 <= x) 
+		{
+			int n7 = 1;
+			while (n7 <= x) 
+			{
+				int result = n3 * n5 * n7;
+				if (result <= x) 
+				{
+					cout << result << endl;
+				}
+				n7 *= 7;
+			}
+			n5 *= 5;
+		}
+		n3 *= 3;
+	}
+};
